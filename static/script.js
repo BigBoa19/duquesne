0c3924053fd546759f3f793cf28a4dc4e6966d58
BigBoa19 <choinoah2005@gmail.com>
2025-03-29 19:37:21 -0400

    modified:   index.html 	modified:   static/script.js 	modified:   static/style.css 	modified:   views/about.html 	modified:   views/gift.html 	modified:   views/plan.html

--- static/script.js
+++ static/script.js
@@ -27,3 +27,83 @@ links.forEach(element => {
         element.classList.add('active');
     }
 });
+
+$(document).ready(function() {
+    $('.subnav-link').removeClass('active');
+    $('.subnav-link:first').addClass('active');
+
+    $('.subnav-link').click(function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+  
+        $('.subnav-link').removeClass('active');
+        $(this).addClass('active');
+  
+        $('.subnav-content').hide();
+        $(target).fadeIn();
+    });
+});
+  
+if(window.location.pathname.endsWith("index.html")){
+    $(document).ready(function(){
+        $("#read-more").click(function(){
+            $("#short-intro").hide();
+            $("#read-more").hide();
+            $("#long-intro").show();
+            $("#read-less").show();
+        });
+        $("#read-less").click(function(){
+            $("#long-intro").hide();
+            $("#read-less").hide();
+            $("#short-intro").show();
+            $("#read-more").show();
+        });
+    });
+}
+
+function validate() {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let phone = document.getElementById("phone");
+    let zip = document.getElementById("zip");
+    let checkbox = document.getElementById("sms-checkbox");
+
+    let emailLength = email.value.length;
+    let emailLengthValid = emailLength > 5 && emailLength < 70;
+
+    let isNumeric = /^\d+$/.test(phone.value);
+
+    let zipValid = zip.value.length === 5 && zip.value.match(/^\d{5}$/);
+
+    let validity = document.getElementById("validity-msg");
+    let failed = false;
+
+    if(!isNumeric && phone.value.length > 0){
+        validity.innerHTML = "Please enter a valid phone number"
+        failed = true;
+    }
+    if(!emailLengthValid){
+        validity.innerHTML = "Please enter a valid email address"
+        failed = true;
+    }
+    if(!zipValid && zip.value.length > 0){
+        validity.innerHTML = "Please enter a valid zip code"
+        failed = true;
+    }
+    if(!name.checkValidity() || !email.checkValidity()){
+        validity.innerHTML = "Please fill out all required fields"
+        failed = true;
+    }
+    
+    if(!failed){
+        validity.innerHTML = "Form submitted successfully";
+        name.value = "";
+        email.value = "";
+        phone.value = "";
+        zip.value = "";
+        checkbox.checked = false;
+        setTimeout(function(){
+            validity.innerHTML = "";
+        }, 2000);
+    }
+}
